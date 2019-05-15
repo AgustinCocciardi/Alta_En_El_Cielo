e0eb0e5ccacc05074cc8f1387af5a7b181f6daf2
Agustin <aguscocciardi@gmail.com>
2019-05-15 16:31:39 -0300

    ...

--- Alta en el cielo/src/paquete/Bandera.java
+++ Alta en el cielo/src/paquete/Bandera.java
@@ -2,9 +2,33 @@ package paquete;
 
 public class Bandera {
 	
-	private int longitud;
+	private int longitudProximoRetazo;
+	private int longitudTotal;
 	private int retazos;
 	
+	public Bandera(int longitudRetazo) {
+		this.longitudProximoRetazo = longitudRetazo;
+		this.longitudTotal = 0;
+		this.retazos = 0;
+	}
 	
+	public int getLongitudProximoRetazo() {
+		return this.longitudProximoRetazo;
+	}
+	
+	private int obtenerProximaLongitud() {
+		int numero = 0, i = 0;
+		String retazo = Integer.toString(longitudProximoRetazo);
+		while( i < retazo.length() ) {
+			numero += retazo.charAt(i++);
+		}
+		return numero;
+	}
+	
+	public void cocerRetazo() {
+		this.retazos++;
+		this.longitudTotal += this.longitudProximoRetazo;
+		this.longitudProximoRetazo += this.obtenerProximaLongitud();
+	}
 	
 }
--- Alta en el cielo/src/paquete/Concurso.java
+++ Alta en el cielo/src/paquete/Concurso.java
@@ -3,8 +3,22 @@ package paquete;
 public class Concurso {
 	
 	private int largoCarretel;
-	private Bandera[] escuelas;
+	private Bandera[] banderas;
 	
+	public Concurso(int largoCarretel, int escuelas, int []retazos) {
+		this.largoCarretel = largoCarretel;
+		banderas = new Bandera[escuelas];
+		for(int i =0; i<escuelas; i++) {
+			this.banderas[i] = new Bandera(retazos[i]);
+		}
+	}
 	
+	public void resolver() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 	
 }
