a2fc1812dbd6023ba86a6e5213a35463174333fa
Agustin <aguscocciardi@gmail.com>
2019-05-17 18:21:03 -0300

    Resolucion en proceso

--- Alta en el cielo/src/paquete/Bandera.java
+++ Alta en el cielo/src/paquete/Bandera.java
@@ -1,19 +1,31 @@
 package paquete;
 
+import java.util.ArrayList;
+
 public class Bandera {
 	
 	private int longitudProximoRetazo;
 	private int longitudTotal;
 	private int retazos;
+	private ArrayList<Integer> subsecuencias; 
 	
 	public Bandera(int longitudRetazo) {
 		this.longitudProximoRetazo = longitudRetazo;
 		this.longitudTotal = 0;
 		this.retazos = 0;
+		this.subsecuencias = new ArrayList<Integer>();
 	}
 	
 	public int getLongitudProximoRetazo() {
 		return this.longitudProximoRetazo;
+	}
+	
+	public int getLongitudTotal() {
+		return this.longitudTotal;
+	}
+	
+	public int getCantidadRetazos() {
+		return this.retazos;
 	}
 	
 	private int obtenerProximaLongitud() {
@@ -28,7 +40,10 @@ public class Bandera {
 	public void cocerRetazo() {
 		this.retazos++;
 		this.longitudTotal += this.longitudProximoRetazo;
+		this.subsecuencias.add(this.longitudProximoRetazo);
 		this.longitudProximoRetazo += this.obtenerProximaLongitud();
 	}
+	
+	
 	
 }
--- Alta en el cielo/src/paquete/Concurso.java
+++ Alta en el cielo/src/paquete/Concurso.java
@@ -1,24 +1,83 @@
 package paquete;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Concurso {
 	
 	private int largoCarretel;
 	private Bandera[] banderas;
 	
-	public Concurso(int largoCarretel, int escuelas, int []retazos) {
-		this.largoCarretel = largoCarretel;
-		banderas = new Bandera[escuelas];
-		for(int i =0; i<escuelas; i++) {
-			this.banderas[i] = new Bandera(retazos[i]);
+	public Concurso(Scanner entrada) {
+		this.largoCarretel = entrada.nextInt();
+		this.banderas = new Bandera[entrada.nextInt()];
+		for(int i=0; i<this.banderas.length; i++) {
+			this.banderas[i] = new Bandera(entrada.nextInt());
 		}
 	}
 	
-	public void resolver() {
-		
+	public int getCarretel() {
+		return this.largoCarretel;
 	}
 	
-	public static void main(String[] args) {
-		
+	private void cocerBanderas() {
+		boolean puedeCocer = true;
+		int noPuedeCocer = 0;
+		int i=0;
+		while(puedeCocer == true) {
+			if(this.largoCarretel > this.banderas[i].getLongitudProximoRetazo()) {
+				this.largoCarretel -= this.banderas[i].getLongitudProximoRetazo();
+				this.banderas[i].cocerRetazo();
+			}
+			else {
+				noPuedeCocer++;
+			}
+			i++;
+			if(i == this.banderas.length)
+				i = 0;
+			if(noPuedeCocer == this.banderas.length)
+				puedeCocer = false;
+		}
+	}
+	
+	private int escuelaConBanderaMasLarga() {
+		int posMayor=0;
+		for(int i=1; i<this.banderas.length; i++) {
+			if(this.banderas[i].getLongitudTotal() > this.banderas[posMayor].getLongitudTotal()) 
+				posMayor = i;
+		}
+		return posMayor+1;
+	}
+	
+	private int escuelaConBanderaConMasRetazos() {
+		int posMasRetazos=0;
+		for(int i=1; i<this.banderas.length; i++) {
+			if(this.banderas[i].getCantidadRetazos() > this.banderas[posMasRetazos].getCantidadRetazos()) 
+				posMasRetazos = i;
+		}
+		return posMasRetazos+1;
+	}
+	
+	public void resolver(PrintWriter salida) {
+		this.cocerBanderas();
+		int escuelaGanadora = this.escuelaConBanderaMasLarga();
+		salida.println(escuelaGanadora + " " + this.banderas[escuelaGanadora].getLongitudTotal());
+		salida.println(this.escuelaConBanderaConMasRetazos());
+		salida.println(this.getCarretel());
+		//
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Scanner entrada = new Scanner(new FileReader("bandera.in"));
+		Concurso concurso = new Concurso(entrada);
+		entrada.close();
+		PrintWriter salida = new PrintWriter(new FileWriter("bandera.out"));
+		concurso.resolver(salida);
+		salida.close();
 	}
 	
 }
